f3b54a669ccb8eedb9e89da846753a4fe6383d01
Daniel Santos <danyv7818@gmail.com>
2022-05-29 11:58:45 -0300

    adição testes concluidos

---
+++ padronizar.py
@@ -0,0 +1,31 @@
+class Palindromo:
+
+  palin = "SOCORRAM, ME SUBI NO ONIBUS EM MARROCOS!"
+  
+  def __init__(self):
+    self.palin
+
+  def tirar_espacos(self):
+    self.palin = self.palin.replace(" ","")
+    return self.palin
+
+  def tirar_virgulas(self):
+    self.palin = self.palin.replace(",","")
+    return self.palin
+
+  def tirar_exclamação(self):
+    self.palin = self.palin.replace("!","")
+    return self.palin
+  
+  def lower_case(self):
+    self.palin = self.palin.lower()
+    return self.palin
+
+  def padronizar():
+    novo = Palindromo()
+    palin = novo.tirar_espacos()
+    palin = novo.tirar_virgulas()
+    palin = novo.tirar_exclamação()
+    palin = novo.lower_case()
+    return palin
+
--- test_palindromo.py
+++ test_palindromo.py
@@ -1,1 +1,34 @@
-#teste para palindromos
+#arquivo de testes para o desafio de palindrom
+
+from padronizar import Palindromo
+import pytest
+
+palin = Palindromo.padronizar()
+
+#testar se é um Palindromo verificando se a ultima letra é igual a primeira 
+def test_End_Start():
+  if (palin[0]) == (palin[-1]):
+    resul = True
+  else:
+    resul = False
+
+  assert resul
+#testar minimo de caracteres 
+def test_size_minimum():
+  if len(palin) > 0:
+    resul = True
+  else:
+    resul = False
+
+  assert resul
+
+def test_verificar_oposto():
+  palin_reverse = ''.join(reversed(palin))
+  assert palin_reverse == palin
+def test_size_limit():
+  if len(palin) <= 1000:
+    resul = True
+  else:
+    resul = False
+
+  assert resul
